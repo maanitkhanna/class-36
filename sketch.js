9d9cd544642951674f157026e68147b6468d8e6e
maanitkhanna <73214498+maanitkhanna@users.noreply.github.com>
2020-12-09 11:37:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var database;
+
+var gameState=0;
+var playerCount=0;
+
+var game,form,player;
+
+
+function setup(){
+    createCanvas(500,500);
+    database= firebase.database();
+    game= new Game();
+    game.getState();
+    game.start();  
+}
+
+function draw(){
+    background("white");
+    
+    drawSprites();
+}
+
